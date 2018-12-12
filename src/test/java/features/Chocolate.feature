ee0b88e89f3e951a2e5facd05c23d9ca2e731d5d
Mayank Gupta <mayank.gupta@qainfotech.com>
2018-12-12 18:19:00 +0530

    Updated

--- src/test/java/features/Chocolate.feature
+++ src/test/java/features/Chocolate.feature
@@ -24,3 +24,39 @@ Feature: Chocolate
     |total|give|eat|share|remaining|
     |300  |50  |72 |25   |   153 |
    
+   
+  Scenario: Basket is empty
+    Given A basket of chocolate bar
+    When The basket has 0 chocolate
+    Then I give 0 chocolate bar as a treat
+    And Friends eat 0 chocolate bar
+    And I share 0 chocolate bar with co-worker
+    And I keep remaining 0 chocolate bar in storage
+    
+  Scenario: All chocolate given as treat
+    Given A basket of 300 chocolate bar
+    When I give 300 chocolate bar as a treat
+    Then Friends eat 0 chocolate bar
+    And I share 0 chocolate bar with co-worker
+    And I keep remaining 0 chocolate bar in storage
+    
+  Scenario: All chocolate eaten by friends
+    Given A basket of 300 chocolate bar
+    When Friends eat 300 chocolate bar
+    Then I give 0 chocolate bar as a treat
+    And I share 0 chocolate bar with co-worker
+    And I keep remaining 0 chocolate bar in storage
+    
+  Scenario: All chocolate shared with co-workers
+    Given A basket of 300 chocolate bar
+    When I share 300 chocolate bar with co-worker
+    Then I give 0 chocolate bar as a treat
+    And Friends eat 0 chocolate bar
+    And I keep remaining 0 chocolate bar in storage
+    
+  Scenario: All chocolate kept in storage
+    Given A basket of 300 chocolate bar
+    When I keep remaining 300 chocolate bar in storage 
+    Then I give 0 chocolate bar as a treat
+    And Friends eat 0 chocolate bar
+    And I share 0 chocolate bar with co-worker
--- src/test/java/stepdefs/Chocolate.java
+++ src/test/java/stepdefs/Chocolate.java
@@ -4,34 +4,60 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
+import static org.junit.Assert.*;
 
 public class Chocolate {
+	
+	private int total_chocolate;
+	private int given_chocolate;
+	private int eaten_chocolate;
+	private int shared_chocolate;
+	private int remaining_chocolate;
+	
 
     @Given("^A basket of (\\d+) chocolate bar$")
     public void a_basket_of_total_chocolate_bar(int total){
+    	total_chocolate = total;
         System.out.format("A basket of %n chocolate bar", total);
     }
     
     @When("^I give (\\d+) chocolate bar as a treat$")
     public void i_give_give_chocolate_bar_as_a_treat(int give){
+    	given_chocolate = give;
     	System.out.format("I give %n chocolate bar as a treat", give);
     }
     
     @When("^Friends eat (\\d+) chocolate bar$")
     public void friends_eat_eat_chocolate_bar(int eat) {
+    	eaten_chocolate = eat;
     	System.out.format("Friends eat %n chocolate bar", eat);
     }
     
     @When("^I share (\\d+) chocolate bar with co-worker$")
     public void i_share_share_chocolate_bar_with_co_worker(int share){
+    	shared_chocolate = share;
     	System.out.format("I share %n chocolate bar with co-worker", share);
     }
     
     @Then("^I keep remaining (\\d+) chocolate bar in storage$")
     public void i_keep_remaining_remaining_chocolate_bar_in_storage(int remaining){
+    	remaining_chocolate = total_chocolate - given_chocolate - eaten_chocolate - shared_chocolate;
+    	assertTrue(remaining_chocolate==remaining);
     	System.out.format("I keep remaining %n chocolate bar in storage", remaining);
     }
     
+    @Given("^A basket of chocolate bar$")
+    public void a_basket_of_chocolate_bar() {
+    	System.out.format("A basket of chocolate bar");
+    }
 
+    @When("^The basket has (\\d+) chocolate$")
+    public void the_basket_has_chocolate(int total){
+    	if(total==0) {
+    		given_chocolate=0;
+    		eaten_chocolate=0;
+    		shared_chocolate=0;
+    		remaining_chocolate=0;
+    	}
+    }
 }
